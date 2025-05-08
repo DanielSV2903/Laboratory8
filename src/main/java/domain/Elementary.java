7cfadbe00abaae3da031e3b316b9c0dfcc3295bc
DanielSV2903 <danielpat433@gmail.com>
2025-05-07 20:23:55 -0600

    Commit clase

--- src/main/java/domain/Elementary.java
+++ src/main/java/domain/Elementary.java
@@ -5,13 +5,36 @@
  */
 package domain;
 
+import util.Utility;
+
 /**
  *
  * @author Profesor Lic. Gilberth Chaves A.
  */
 public class Elementary {
+    public static int getTotalChanges() {
+        return TotalChanges;
+    }
+
+    public static int getTotalIterations() {
+        return TotalIterations;
+    }
+
+    public static void setTotalChanges(int totalChanges) {
+        TotalChanges = totalChanges;
+    }
+
+    public static void setTotalIterations(int totalIterations) {
+        TotalIterations = totalIterations;
+    }
+
     private static int TotalChanges;
     private static int TotalIterations;
+    private static int[] CounterArray;
+
+    public static int[] getCounterArray() {
+        return CounterArray;
+    }
 
     public static void bubbleSort(int a[]){
         TotalIterations=0;
@@ -23,27 +46,34 @@ public class Elementary {
                     int aux=a[j];
                     a[j]=a[j+1];
                     a[j+1]=aux;
+                    TotalChanges++;
                 }//if
             }//for j
         }
     }
     
     public static void improvedBubbleSort(int a[]){
+        TotalIterations=0;
+        TotalChanges=0;
 	    boolean swapped = true; //intercambiado
 	    for(int i=1;swapped;i++){
             swapped = false;
+            TotalIterations++;
             for(int j=0;j<a.length-i;j++){
                 if(a[j]>a[j+1]){
                     int aux=a[j];
                     a[j]=a[j+1];
                     a[j+1]=aux;
                     swapped = true;
+                    TotalChanges++;
                 }//if
             }//for j
 	    }//for i
     }
     
     public static void selectionSort(int a[]){
+        TotalIterations=0;
+        TotalChanges=0;
         for(int i=0;i<a.length-1;i++){
             int min=a[i];
             int minIndex=i;
@@ -51,14 +81,18 @@ public class Elementary {
                 if(a[j]<min){
                     min=a[j];
                     minIndex=j;
+                    TotalChanges++;
                 }//if
             }//for j
             a[minIndex]=a[i];
             a[i]=min;
+            TotalIterations++;
         }//for i
     }
     
     public static void countingSort(int a[]) {
+        TotalIterations=0;
+        TotalChanges=0;
         int max = util.Utility.maxArray(a); //va de 0 hasta el elemento maximo
         // create buckets
         int counter[] = new int[max + 1];
@@ -66,12 +100,15 @@ public class Elementary {
         for (int element : a) {
             counter[element]++; //incrementa el num de ocurrencias del elemento
         }
+        CounterArray= Utility.copyArray(counter);
         // sort array
         int index = 0;
         for (int i = 0; i < counter.length; i++) {
+            TotalIterations++;
             while (counter[i]>0) { //significa q al menos hay un elemento (q existe)
                 a[index++] = i;
                 counter[i]--;
+                TotalChanges++;
             }
         }//for i
     }
--- src/test/java/domain/SortingTest.java
+++ src/test/java/domain/SortingTest.java
@@ -1,7 +1,66 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
+import util.Utility;
+
+import javax.print.DocFlavor;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortingTest {
+    private String elementarySorting(String algorithm,int[] a,int n){
+        StringBuilder result=new StringBuilder();
+        int copyOf[]= Utility.copyArray(a);
+        switch (algorithm){
+            case "bubble":
+                Elementary.bubbleSort(a);
+                break;
+            case "improvedBubble":
+                Elementary.improvedBubbleSort(a);
+                break;
+            case "selectionSort":
+                Elementary.selectionSort(a);
+                break;
+            case "countingSort":
+                Elementary.countingSort(a);
+                break;
+        }
+        result.append("\n"+algorithm +"- Test\n" +
+                "Algorithm: "+algorithm +"\n" +
+                "Original Array: ");
+        result.append(arrayToString(copyOf,copyOf.length));
+        result.append("\nSorted Array: "+arrayToString(a,n));
+        if (algorithm.equals("countingSort")){
+            result.append("\nCounter Array: "+arrayToString(Elementary.getCounterArray(),a.length));
+        }else {
+        result.append("\nTotal Changes: "+Elementary.getTotalChanges());
+        result.append("\nTotal Iterations: "+Elementary.getTotalIterations());
+        }
+        return result.toString();
+    }
+
+    private String arrayToString(int[] a,int n) {
+        StringBuilder builder=new StringBuilder();
+        builder.append("{");
+        for (int i=0;i<n;i++){
+            builder.append(a[i]+",");
+        }
+        builder.append("}");
+        return builder.toString();
+    }
+
+    @Test
+    void elementaryTest(){
+    int[] a = util.Utility.getIntegerArray(1000);
+    int[] b = util.Utility.copyArray(a);
+    System.out.println(elementarySorting("bubble",a,50));
+    System.out.println(elementarySorting("improvedBubble",b,100));
+    System.out.println(elementarySorting("selectionSort",util.Utility.getIntegerArray(1000),150));
+    System.out.println(elementarySorting("countingSort",util.Utility.getIntegerArray(1000),200));
+
+
+
+
+    }
 
 }
