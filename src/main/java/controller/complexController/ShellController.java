b8302e014090a8bfdec47579142bce708dedc5d2
Daniel Sánchez Vargas <danielpat433@gmail.com>
2025-05-08 22:34:44 -0600

    FXML Carga

--- src/main/java/controller/complexController/ShellController.java
+++ src/main/java/controller/complexController/ShellController.java
@@ -1,7 +1,45 @@
 package controller.complexController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class ShellController
 {
     @javafx.fxml.FXML
+    private TextField gapSubArray1TextField;
+    @javafx.fxml.FXML
+    private TextField arrayLengthTextField;
+    @javafx.fxml.FXML
+    private TableView sortedArrayTableVIew;
+    @javafx.fxml.FXML
+    private TextField gapSubArray3TextField;
+    @javafx.fxml.FXML
+    private TableView noSortedArrayTableVIew;
+    @javafx.fxml.FXML
+    private TextField lowBoundTextField;
+    @javafx.fxml.FXML
+    private TextField gapSubArray2TextField;
+    @javafx.fxml.FXML
+    private TextField gapNTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void startOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void randomizeOnAction(ActionEvent actionEvent) {
+    }
+}
