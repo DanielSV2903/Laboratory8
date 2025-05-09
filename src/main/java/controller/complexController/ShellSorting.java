fe92d0260a676bbd569023a1509895a50370bb52
chocol3it <murillosojoandree@gmail.com>
2025-05-08 22:10:48 -0600

    Controllers

---
+++ src/main/java/controller/complexController/ShellSorting.java
@@ -0,0 +1,54 @@
+package controller.complexController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ShellSorting {
+
+    @FXML
+    private TextField arrayLengthTextField;
+
+    @FXML
+    private TextField gapNTextField;
+
+    @FXML
+    private TextField gapSubArray1TextField;
+
+    @FXML
+    private TextField gapSubArray2TextField;
+
+    @FXML
+    private TextField gapSubArray3TextField;
+
+    @FXML
+    private TextField lowBoundTextField;
+
+    @FXML
+    private TableView<?> noSortedArrayTableVIew;
+
+    @FXML
+    private TableView<?> sortedArrayTableVIew;
+
+    @FXML
+    void clearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void randomizeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startOnAction(ActionEvent event) {
+
+    }
+
+}
