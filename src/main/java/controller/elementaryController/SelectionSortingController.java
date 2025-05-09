198261824ad75f8970d885fae562cef65483c033
FortGamer207 <tatanmc2007@gmail.com>
2025-05-09 13:32:19 -0600

    SelectionSorting terminado

--- src/main/java/controller/elementaryController/SelectionSortingController.java
+++ src/main/java/controller/elementaryController/SelectionSortingController.java
@@ -1,12 +1,11 @@
 package controller.elementaryController;
 
+import domain.Elementary;
 import javafx.beans.Observable;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import util.Utility;
 
@@ -52,10 +51,20 @@ public class SelectionSortingController
         noSortedArrayTableVIew.getItems().clear();
         noSortedArrayTableVIew.getColumns().clear();
 
-        for (int i = 0; i < Integer.parseInt(arrayLengthTextField.getText().trim()); i++) {
-            noSortedArrayTableVIew.getColumns().add(new javafx.scene.control.TableColumn<>("[" + i + "]"));
-            
-        }
+        int [] noSortedArray = Utility.createArray(arrayLengthTextField.getText().trim(),lowBoundTextField.getText().trim(),highBoundTextField.getText().trim());
+        int [] sortedArray = Utility.copyArray(noSortedArray);
+
+        crearTV(noSortedArrayTableVIew, noSortedArray.length);
+        updateTV(noSortedArrayTableVIew, noSortedArray);
+        Elementary.selectionSort(sortedArray);
+
+        crearTV(sortedArrayTableView, sortedArray.length);
+        updateTV(sortedArrayTableView, sortedArray);
+
+        minValueTextField.setText(sortedArray[0]+"");
+        maxValueTextField.setText(sortedArray[sortedArray.length-1]+"");
+        iterationTextField.setText(Elementary.getTotalIterations()+"");
+        changesTextField.setText(Elementary.getTotalChanges()+"");
 
         mostrarAlerta("Valores ingresados correctamente");
 
@@ -70,9 +79,54 @@ public class SelectionSortingController
 
     @javafx.fxml.FXML
     public void clearOnAction(ActionEvent actionEvent) {
+        noSortedArrayTableVIew.getItems().clear();
+        noSortedArrayTableVIew.getColumns().clear();
+        sortedArrayTableView.getItems().clear();
+        sortedArrayTableView.getColumns().clear();
+        minValueTextField.clear();
+        iterationTextField.clear();
+        changesTextField.clear();
+        arrayLengthTextField.clear();
+        maxValueTextField.clear();
     }
 
     @javafx.fxml.FXML
     public void randomizeOnAction(ActionEvent actionEvent) {
+        int arrayLength = Utility.random(1, 200);
+        int lowBound = Utility.random(0, 50);
+        int highBound = Utility.random(lowBound+1, 100);
+
+        int[] noSortedArray = Utility.createArray(String.valueOf(arrayLength),String.valueOf(lowBound),String.valueOf(highBound));
+        int[] sortedArray = Utility.copyArray(noSortedArray);
+
+        crearTV(noSortedArrayTableVIew, noSortedArray.length);
+        updateTV(noSortedArrayTableVIew, noSortedArray);
+        Elementary.selectionSort(sortedArray);
+
+        crearTV(sortedArrayTableView, sortedArray.length);
+        updateTV(sortedArrayTableView, sortedArray);
+    }
+
+    private void crearTV(TableView<ObservableList<SimpleIntegerProperty>> tableView, int length) {
+        tableView.getColumns().clear();
+
+        for (int i = 0; i < length; i++) {
+            final int colIndex = i;
+            TableColumn<ObservableList<SimpleIntegerProperty>, Number> column =
+                    new TableColumn<>("[" + i + "]");
+            column.setCellValueFactory(data -> data.getValue().get(colIndex));
+            tableView.getColumns().add(column);
+        }
+    }
+
+    private void updateTV(TableView<ObservableList<SimpleIntegerProperty>> tableView, int[] arreglo) {
+        tableView.getItems().clear();
+
+        ObservableList<SimpleIntegerProperty> fila = javafx.collections.FXCollections.observableArrayList();
+        for (int value : arreglo) {
+            fila.add(new SimpleIntegerProperty(value));
+        }
+
+        tableView.getItems().add(fila);
     }
 }
